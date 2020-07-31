3a610a454f89e94ead501662ca5ac159e62a99b1
bielb1b2 <gabrielvt.souza@hotmail.com>
2020-07-31 20:55:27 -0300

    pagina inicial funcionando

--- lib/tabs/fake_tab.dart
+++ lib/tabs/fake_tab.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:app_violencia/tabs/help_tab.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
@@ -6,20 +8,70 @@ class FakeTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final List<Widget> containers = [
-      Container(),
+      CustomScrollView(
+        primary: false,
+        slivers: <Widget>[
+          SliverPadding(
+            padding: const EdgeInsets.all(1.0),
+            sliver: SliverGrid.count(
+              crossAxisCount: 1,
+              crossAxisSpacing: 1.0,
+              children: <Widget>[
+                Container(
+                  padding: const EdgeInsets.all(1.0),
+                  child: Column(
+                    children: <Widget>[
+                      Text(
+                        "PASTEL",
+                        style: TextStyle(
+                            fontSize: 30.0, fontWeight: FontWeight.bold),
+                      ),
+                      Image.asset('assets/pastel.jpg'),
+                    ],
+                  ),
+                ),
+                Container(
+                  child: Column(
+                    children: <Widget>[
+                      Text(
+                        "JAPONÊS",
+                        style: TextStyle(
+                            fontSize: 30.0, fontWeight: FontWeight.bold),
+                      ),
+                      Image.asset('assets/japanesefood.png'),
+                    ],
+                  ),
+                ),
+                Container(
+                  child: Column(
+                    children: <Widget>[
+                      Text(
+                        "PIZZA",
+                        style: TextStyle(
+                            fontSize: 30.0, fontWeight: FontWeight.bold),
+                      ),
+                      Image.asset('assets/pizza.jpg'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
       Container(
-          child: Center(
-        child: Text(
-          "Você ainda não adicionou nada ao seu carrinho. 😣",
-          style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
-          textAlign: TextAlign.center,
+        child: Center(
+          child: Text(
+            "Você ainda não adicionou nada ao seu carrinho. 😣",
+            style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
         ),
-      )),
-      HelpTab(),
+      ),
     ];
 
     return DefaultTabController(
-      length: 3,
+      length: 2,
       child: Scaffold(
         appBar: AppBar(
           title: Text(
@@ -39,14 +91,17 @@ class FakeTab extends StatelessWidget {
               Tab(
                 text: "Carrinho",
               ),
-              Tab(
-                text: "Ajuda",
-              ),
             ],
           ),
         ),
         body: TabBarView(
           children: containers,
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.purple[800],
+          child: Container(
+            height: 50.0,
+          ),
         ),
       ),
     );
