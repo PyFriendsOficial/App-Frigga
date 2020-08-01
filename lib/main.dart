7b6647910b481c1101b5d3fa54baed5232c8874c
Rudney Eduardo <rudneyeduardo.souza@gmail.com>
2020-07-31 21:54:06 -0300

    Subindo um detalhe de nome

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Frigga',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
