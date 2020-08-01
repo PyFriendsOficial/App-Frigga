8404e5c36885de8d32411dd7ef00159c9c18a6b2
Rudney Eduardo <rudneyeduardo.souza@gmail.com>
2020-07-31 21:41:22 -0300

    Versão de teste e apresentação terminada

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -8,21 +10,61 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+
+  Set<Marker> _markers = HashSet<Marker>();
+
+  
+
   GoogleMapController mapController;
 
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+  final LatLng _center = const LatLng(-23.5625871, -46.6552981);
+
+  final LatLng _center1 = const LatLng(-23.5583136, -46.6510289);
+
+  final LatLng _center2 = const LatLng(-23.555485, -46.6540854);
+
+  GoogleMapController _mapController;
 
   void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
+    _mapController = controller;
+
+    setState(() {
+      _markers.add(Marker(
+        markerId: MarkerId("0"),
+        position: _center,
+        infoWindow: InfoWindow(
+          title: "Av. Paulista, 1450, São Paulo",
+          snippet: "Confusão Domiciliar"
+          ),
+        ));
+        _markers.add(Marker(
+        markerId: MarkerId("1"),
+        position: _center1,
+        infoWindow: InfoWindow(
+          title: "R. Itapeva, 142, São Paulo",
+          snippet: "Agressão de familiar"
+          ),
+        ));
+        _markers.add(Marker(
+        markerId: MarkerId("2"),
+        position: _center2,
+        infoWindow: InfoWindow(
+          title: "R. Frei Caneca, 569, São Paulo",
+          snippet: "Mulher Sofrendo Violência Física"
+          ),
+        ));
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Maps Sample App'),
-          backgroundColor: Colors.green[700],
+          centerTitle: true,
+          title: Text('HeatMap'),
+          backgroundColor: Colors.purple,
         ),
         body: GoogleMap(
           onMapCreated: _onMapCreated,
@@ -30,6 +72,7 @@ class _MapPageState extends State<MapPage> {
             target: _center,
             zoom: 11.0,
           ),
+          markers: _markers,
         ),
       ),
     );
